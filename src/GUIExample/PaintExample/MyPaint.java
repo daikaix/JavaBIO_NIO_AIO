80874d75c047dee7248fecec0d0c904643397af3
daikaix <996327890@qq.com>
2021-06-14 22:21:08 +0800

    Paint画笔

---
+++ src/GUIExample/PaintExample/MyPaint.java
@@ -0,0 +1,36 @@
+package GUIExample.PaintExample;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class MyPaint extends Frame {
+    MyPaint(){
+        setSize(500,500);
+        setVisible(true);
+        pack();
+        windowClose(this);
+    }
+
+    private static void windowClose(Frame frame){
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+
+    @Override
+    public void paint(Graphics g) {
+        g.setColor(new Color(1,1,1));
+        //g.drawOval(100,100,100,100);
+        g.fillOval(100,100,100,100);
+
+        g.setColor(Color.green);
+        g.fillRect(150,200,200,200);
+
+
+    }
+}
---
+++ src/GUIExample/PaintExample/TestPaint.java
@@ -0,0 +1,7 @@
+package GUIExample.PaintExample;
+
+public class TestPaint {
+    public static void main(String[] args) {
+        MyPaint p = new MyPaint();
+    }
+}
