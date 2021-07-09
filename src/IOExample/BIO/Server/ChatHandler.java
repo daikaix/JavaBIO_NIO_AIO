0535681057bab28f2125be6174583a8ff5761b8c
daikaix <996327890@qq.com>
2021-07-09 08:35:47 +0800

    ChatHandler: 获取到客户端Socket 添加进server的信息表中 转发所有信息 关闭，并删除对应信息 ChatServer: 声明一些函数 创建ServerSocket，获取对应Socket，并封装成ChatHandler线程

--- src/IOExample/BIO/Server/ChatHandler.java
+++ src/IOExample/BIO/Server/ChatHandler.java
@@ -1,4 +1,49 @@
 package IOExample.BIO.Server;
 
-public class ChatHandler {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class ChatHandler implements Runnable{
+    private ChatServer server;
+    private Socket socket;
+
+    public ChatHandler(ChatServer server,Socket socket){
+        this.server = server;
+        this.socket = socket;
+    }
+
+
+
+    @Override
+    public void run(){
+        try {
+            server.addClient(socket);
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream())
+            );
+
+            String msg = null;
+            while ((msg = reader.readLine()) != null){
+                String fwdMsg = "客户端["+socket.getPort()+"]:"+msg;
+                System.out.println(fwdMsg);
+                server.forwardMessage(socket,fwdMsg);
+
+                if (server.readyToQuit(msg)){
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                server.removeClient(socket);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
--- src/IOExample/BIO/Server/ChatServer.java
+++ src/IOExample/BIO/Server/ChatServer.java
@@ -2,18 +2,95 @@ package IOExample.BIO.Server;
 
 import IOExample.Socket.Server;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ChatServer {
     private int DEFAULT_PORT = 8888;
     private final String QUIT = "quit";
 
+    private ServerSocket serverSocket;
+    private Map<Integer,Writer> connectedClients;
 
+    private ChatServer(){
+        connectedClients = new HashMap<>();
+    }
+
+    public synchronized void addClient(Socket socket) throws IOException {
+        if(socket != null){
+            int port = socket.getPort();
+            BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(socket.getOutputStream())
+            );
+            connectedClients.put(port,writer);
+            System.out.println(port+"上线！");
+        }
+    }
+
+    public synchronized void removeClient(Socket socket) throws IOException {
+        if (socket!=null){
+            int port = socket.getPort();
+            if (connectedClients.containsKey(port)){
+                connectedClients.get(port).close();
+            }
+            connectedClients.remove(port);
+            System.out.println(port+"下线！");
+        }
+    }
+
+    public synchronized void forwardMessage(Socket socket, String fwdMsg) throws IOException {
+        for (Integer id: connectedClients.keySet()){
+            if (!id.equals(socket.getPort())){
+                Writer writer = connectedClients.get(id);
+                writer.write(fwdMsg);
+                writer.flush();
+            }
+        }
+    }
+
+    public boolean readyToQuit(String msg){
+        if (msg.equals(QUIT)){
+            return true;
+        }
+        return false;
+    }
+
+    public void close(){
+        if (serverSocket!=null){
+            try {
+                serverSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void start(){
+        try {
+            serverSocket = new ServerSocket(DEFAULT_PORT);
+            System.out.println("监听端口");
+
+            while (true){
+                Socket socket = serverSocket.accept();
+                //为其创建ChatHandler线程
+                new Thread(new ChatHandler(this,socket)).start();
+            }
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+        finally {
+            close();
+        }
+    }
+
+    public static void main(String[] args) {
+        ChatServer server = new ChatServer();
+        server.start();
+    }
 }
